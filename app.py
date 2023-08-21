5ccfb68cfdbebab15b451fdfbc28c492735eb55c
Devi <me@example.com>
2023-08-21 12:46:30 -0600

    Complete code for both the files.

--- app.py
+++ app.py
@@ -4,12 +4,11 @@
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func,desc
+from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
 
 import datetime as dt
-import numpy as np
 
 #################################################
 # Database Setup
@@ -23,7 +22,6 @@ Base = automap_base()
 Base.prepare(autoload_with=engine )
 
 # Save references to each table
-#print(Base.classes.keys())
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
@@ -36,7 +34,7 @@ Station = Base.classes.station
 #################################################
 app = Flask(__name__)
 
-# To avoid jsonify() sorting the keys
+# To retain the order of keys in the dictionary passed to jsonify (to avoid jsonify() sorting the keys)
 app.config['JSON_SORT_KEYS'] = False
 
 
@@ -48,19 +46,35 @@ app.config['JSON_SORT_KEYS'] = False
 def home():
     """ List of available API Routes: """
     return (
-        f"Available Routes:<br/>"
+        f"<b>Available Routes: </b><br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<startdate><br/>"
-        f"/api/v1.0/<startdate>/<enddate><br/>"        
+        f"/api/v1.0/2010-01-26<br/>"        
+        f"/api/v1.0/2010-01-26/2015-03-24<br/>"        
+        f"<b>Note: Date format must be: yyyy-mm-dd </b><br/>"
     )
 
-# Function to calculate last year date
+# Function to calculate previous year date
 def previous_year_date():
     
-    # Calculate the date one year from the last date in data set.
-    last_year_date = str(dt.date(2017,8,23) - dt.timedelta(days=365))
+    # Create session
+    session = Session(engine)
+
+    # Get the last/most recent date from the data
+    recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    
+    # Close session
+    session.close()
+
+    # From the recent date, get year, month and date seperately 
+    latest_date  = str(recent_date)
+    r_year = int(latest_date[2:6])
+    r_month = int(latest_date[7:9])
+    r_date = int(latest_date[10:12])
+
+    # Calculate the date one year from the last/most recent date in data set.
+    last_year_date = str(dt.date(r_year,r_month,r_date) - dt.timedelta(days=365))
 
     return last_year_date
 
@@ -68,11 +82,11 @@ def previous_year_date():
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
     # Calculate the date one year from the last date in data set.
     last_year_date = previous_year_date()
+
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
     # Retrieve the data and precipitation scores for the last year.
     last_month_data = session.query(Measurement.date, Measurement.prcp).\
@@ -110,11 +124,11 @@ def stations():
     station_data_list=[]
     for station,name,latitude,longitude,elevation in station_data:
         data_dict = {}
-        data_dict["station"] = station
-        data_dict["name"] = name
-        data_dict["latitude"] = latitude
-        data_dict["longitude"] = longitude
-        data_dict["elevation"] = elevation
+        data_dict["Station ID"] = station
+        data_dict["Station Name"] = name
+        data_dict["Latitude"] = latitude
+        data_dict["Longitude"] = longitude
+        data_dict["Elevation"] = elevation
 
         # Create a list dictionaries
         station_data_list.append(data_dict)
@@ -126,6 +140,9 @@ def stations():
 @app.route("/api/v1.0/tobs")
 def temp_Observ():
 
+    # Calculate the date one year from the last date in data set.
+    last_year_date = previous_year_date()
+
     # Create the session link
     session = Session(engine)
 
@@ -135,12 +152,11 @@ def temp_Observ():
                     group_by(Station.station).\
                     order_by(func.count(Measurement.station).desc()).first()
     
-    most_active_station_id = str(most_active_station)[2:-3]
-    
-    # Calculate the date one year from the last date in data set.
-    last_year_date = previous_year_date()
+    print(most_active_station)
+    # Remove the extra characters and get the ID from the result
+    most_active_station_id = str(most_active_station)[2:-3]    
 
-    # Get the dates and temperature observations of the most-active station for the previous year of data
+    # Get the dates and temperature observations of the most-active station for the previous year
     tobs_data = session.query(Measurement.station, Measurement.date,Measurement.tobs).\
                 filter(Measurement.station == most_active_station_id).\
                 filter(Measurement.date >= last_year_date ).all()
@@ -153,9 +169,9 @@ def temp_Observ():
     tobs_data_list=[]
     for station,date,tobs in tobs_data:
         data_dict = {}
-        data_dict["station"] = station
-        data_dict["date"] = date
-        data_dict["tobs"] = tobs
+        data_dict["Station ID"] = station
+        data_dict["Date"] = date
+        data_dict["Observed temperature"] = tobs
 
         # Create a list dictionaries
         tobs_data_list.append(data_dict)
@@ -164,6 +180,58 @@ def temp_Observ():
     return jsonify(tobs_data_list)
     
 	
+@app.route("/api/v1.0/<startdate>")
+def temp_calc_start_date(startdate):    
+
+    # Create session
+    session = Session(engine)
+
+    # Calculate MIN, MAX, AVG temperature for all the dates greater than or equal to the start date
+    sel = [func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)]
+    temp_data = session.query(*sel).\
+                filter(Measurement.date >= startdate).all()
+        
+    # Close the session
+    session.close()    
+
+    # unpack the values and set in a dictionary
+    min_temp, max_temp, avg_temp = temp_data[0]
+    temp_data_dict = {}
+    if (min_temp is None) and (max_temp is None) and (avg_temp is None):
+        return jsonify({ "Error": f"No data found for startdate {startdate}"}),404
+    else:               
+        temp_data_dict['Minimum temperature'] = min_temp
+        temp_data_dict['Maximum temperature'] = max_temp
+        temp_data_dict['Average temperature'] = round(avg_temp,2)
+        return jsonify(temp_data_dict)
+
+
+@app.route("/api/v1.0/<startdate>/<enddate>")
+def temp_calc_start_end_date(startdate, enddate):    
+
+    # Create session
+    session = Session(engine)
+
+    # Calculate MIN, MAX, AVG temperature for the dates from the start date to the end date, inclusive
+    sel = [func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)]
+    temperature_data = session.query(*sel).\
+                filter(Measurement.date >= startdate).\
+                filter(Measurement.date <= enddate).all()
+    
+    # Close the session
+    session.close()
+        
+    # unpack the values and set in a dictionary
+    min_temp, max_temp, avg_temp = temperature_data[0]
+    temperature_data_dict = {}
+    if (min_temp is None) and (max_temp is None) and (avg_temp is None):
+        return jsonify({ "Error": f"No data found for startdate {startdate} and enddate {enddate}"}),404
+    else:        
+        temperature_data_dict['Minimum temperature'] = min_temp
+        temperature_data_dict['Maximum temperature'] = max_temp
+        temperature_data_dict['Average temperature'] = round(avg_temp,2)
+        return jsonify(temperature_data_dict)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
